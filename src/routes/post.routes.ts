410f1e02864b89c944cddaaa38398529168fc6a8
juanfmange <juanfmange@gmail.com>
2025-07-04 11:13:40 -0600

    sqlLite

--- src/routes/post.routes.ts
+++ src/routes/post.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import prisma from "../lib/prisma";
 import { Post } from "../models/post.model";
 import { generateRSSFeed } from "../services/rss.service";
 
@@ -6,27 +7,43 @@ const router = Router();
 let posts: Post[] = [];
 
 // Crear un nuevo post
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   const { title, content } = req.body;
-  const newPost: Post = {
-    id: posts.length + 1,
-    title,
-    content,
-    date: new Date(),
-  };
-  posts.push(newPost);
+  const newPost = await prisma.post.create({
+    data: { title, content },
+  });
   res.status(201).json(newPost);
 });
+// router.post("/", (req, res) => {
+//   const { title, content } = req.body;
+//   const newPost: Post = {
+//     id: posts.length + 1,
+//     title,
+//     content,
+//     date: new Date(),
+//   };
+//   posts.push(newPost);
+//   res.status(201).json(newPost);
+// });
 
 // Obtener todos los posts
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
+  const posts = await prisma.post.findMany({ orderBy: { date: "desc" } });
   res.json(posts);
 });
+// router.get("/", (req, res) => {
+//   res.json(posts);
+// });
 
 // Obtener RSS feed
-router.get("/rss", (req, res) => {
+router.get("/rss", async (req, res) => {
+  const posts = await prisma.post.findMany({ orderBy: { date: "desc" } });
   const xml = generateRSSFeed(posts);
   res.type("application/rss+xml").send(xml);
 });
+// router.get("/rss", (req, res) => {
+//   const xml = generateRSSFeed(posts);
+//   res.type("application/rss+xml").send(xml);
+// });
 
 export default router;
--- src/services/rss.service.ts
+++ src/services/rss.service.ts
@@ -1,10 +1,11 @@
 import RSS from "rss";
-import { Post } from "../models/post.model";
+import { Post } from "@prisma/client";
+//import { Post } from "../models/post.model";
 
 export function generateRSSFeed(posts: Post[]): string {
   const feed = new RSS({
     title: "Prueba demo RSS",
-    description: "Contenido generado dinámicamente por Juanito",
+    description: "Sindicando contenido desde SQLite (migrar a MS SQL Server",
     feed_url: "http://localhost:3000/rss",
     site_url: "http://localhost:3000",
     language: "es",
